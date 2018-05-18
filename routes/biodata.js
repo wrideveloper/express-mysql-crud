03c3fdfa9230cf0877351ba689cbadbf53bb1de9
mnindrazaka <mnindrazaka@gmail.com>
2018-05-19 04:25:24 +0700

    add database and working CRUD

--- routes/biodata.js
+++ routes/biodata.js
@@ -2,31 +2,39 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('biodata/index')
+  req.con.query('SELECT * FROM biodata', function (err, rows) {
+    console.log(rows)
+    res.render('biodata/index', {data: rows})
+  })
 })
 
 router.get('/create', function (req, res) {
   res.render('biodata/create')
 })
 
-router.post('/', function (req, res) {
-  res.send('menyimpan biodata')
+router.post('/create', function (req, res) {
+  const biodata = req.body;
+  req.con.query(`INSERT INTO biodata SET nama = '${biodata.nama}', alamat = '${biodata.alamat}'`, function (err) {
+    res.redirect('/biodata')
+  })
 })
 
-router.get('/:id', function (req, res) {
-  res.render('biodata/show')
+router.get('/edit/:id', function (req, res) {
+  req.con.query(`SELECT * FROM biodata WHERE id_biodata = ${req.params.id}`, function (err, rows) {
+    res.render('biodata/edit', {data: rows[0]})
+  })
 })
 
-router.get('/:id/edit', function (req, res) {
-  res.render('biodata/edit')
+router.post('/edit/:id', function (req, res) {
+  const biodata = req.body;
+  req.con.query(`UPDATE biodata SET nama = '${biodata.nama}', alamat = '${biodata.alamat}' WHERE id_biodata = ${req.params.id}`, function (err) {
+    res.redirect('/biodata')
+  })
 })
 
-router.put('/:id', function (req, res) {
-  res.send('update biodata')
+router.get('/delete/:id', function (req, res) {
+  req.con.query(`DELETE FROM biodata WHERE id_biodata = ${req.params.id}`, function (err) {
+    res.redirect('/biodata')
+  })
 })
-
-router.delete('/:id', function (req, res) {
-  res.send('delete biodata')
-})
-
 module.exports = router
--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express()
 const mysql = require('mysql')
 const path = require('path')
+const bodyParser = require('body-parser')
+require('dotenv').config()
 
 // include router
 const biodataRouter = require('./routes/biodata')
 
 // create mysql connection
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'biodata'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 })
 con.connect(function (err) {
   if (err) {
@@ -23,13 +25,17 @@ con.connect(function (err) {
 
 // Using EJS template engine
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('view engine', 'pug')
 
 // connecting route to database
 app.use(function (req, res, next) {
   req.con = con
   next()
 })
+
+// parsing post data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // routing
 app.use('/biodata', biodataRouter)
