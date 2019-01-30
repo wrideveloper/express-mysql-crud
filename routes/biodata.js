333499a31f63cd058f7d66b4a0bf3c8f07ff1adf
mnindrazaka <mnindrazaka@gmail.com>
2019-01-30 12:00:51 +0700

    add method override

--- routes/biodata.js
+++ routes/biodata.js
@@ -1,40 +1,57 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
-router.get('/', function (req, res) {
-  req.con.query('SELECT * FROM biodata', function (err, rows) {
-    console.log(rows)
-    res.render('biodata/index', {data: rows})
+router.get("/", function(req, res) {
+  req.con.query("SELECT * FROM biodata", function(err, rows) {
+    res.render("biodata/index", { data: rows })
   })
 })
 
-router.get('/create', function (req, res) {
-  res.render('biodata/create')
+router.get("/create", function(req, res) {
+  res.render("biodata/create")
 })
 
-router.post('/create', function (req, res) {
-  const biodata = req.body;
-  req.con.query(`INSERT INTO biodata SET nama = '${biodata.nama}', alamat = '${biodata.alamat}'`, function (err) {
-    res.redirect('/biodata')
-  })
+router.post("/", function(req, res) {
+  const biodata = req.body
+  req.con.query(
+    `INSERT INTO biodata SET 
+      nama = '${biodata.nama}', 
+      alamat = '${biodata.alamat}'`,
+    function(err) {
+      res.redirect("/biodata")
+    }
+  )
 })
 
-router.get('/edit/:id', function (req, res) {
-  req.con.query(`SELECT * FROM biodata WHERE id_biodata = ${req.params.id}`, function (err, rows) {
-    res.render('biodata/edit', {data: rows[0]})
-  })
+router.get("/:id/edit", function(req, res) {
+  req.con.query(
+    `SELECT * FROM biodata WHERE id_biodata = ${req.params.id}`,
+    function(err, rows) {
+      res.render("biodata/edit", { data: rows[0] })
+    }
+  )
 })
 
-router.post('/edit/:id', function (req, res) {
-  const biodata = req.body;
-  req.con.query(`UPDATE biodata SET nama = '${biodata.nama}', alamat = '${biodata.alamat}' WHERE id_biodata = ${req.params.id}`, function (err) {
-    res.redirect('/biodata')
-  })
+router.put("/:id", function(req, res) {
+  const biodata = req.body
+  req.con.query(
+    `UPDATE biodata SET 
+      nama = '${biodata.nama}', 
+      alamat = '${biodata.alamat}' 
+      WHERE id_biodata = ${req.params.id}`,
+    function(err) {
+      res.redirect("/biodata")
+    }
+  )
 })
 
-router.get('/delete/:id', function (req, res) {
-  req.con.query(`DELETE FROM biodata WHERE id_biodata = ${req.params.id}`, function (err) {
-    res.redirect('/biodata')
-  })
+router.delete("/:id", function(req, res) {
+  req.con.query(
+    `DELETE FROM biodata WHERE id_biodata = ${req.params.id}`,
+    function(err) {
+      res.redirect("/biodata")
+    }
+  )
 })
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,45 +1,33 @@
-const express = require('express');
+const express = require("express")
 const app = express()
-const mysql = require('mysql')
-const path = require('path')
-require('dotenv').config()
-
-// include router
-const biodataRouter = require('./routes/biodata')
+const methodOverride = require("method-override")
+const path = require("path")
 
 // create mysql connection
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-})
-con.connect(function (err) {
-  if (err) {
-    console.log('database connection error')
-  } else {
-    console.log('database connection success')
-  }
-})
+const con = require("./config/db")
 
 // Using pug template engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
 
 // connecting route to database
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   req.con = con
   next()
 })
 
-// parsing post data
+// parsing body request
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+
+// include router
+const biodataRouter = require("./routes/biodata")
 
 // routing
-app.use('/biodata', biodataRouter)
+app.use("/biodata", biodataRouter)
 
 // starting server
-app.listen(3000, function () {
-  console.log('server listening on port 3000')
+app.listen(3000, function() {
+  console.log("server listening on port 3000")
 })
